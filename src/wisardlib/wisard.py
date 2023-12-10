dadb36059bb76eefab7db1d96f3fcc27f5db9bf8
Otavio Napoli <otavio.napoli@gmail.com>
2023-12-10 00:32:04 +0000

    Fixed Wisard.fit

--- src/wisardlib/wisard.py
+++ src/wisardlib/wisard.py
@@ -388,8 +388,7 @@ class WiSARD:
         for i in it:
             # Transform each input as a list of subsamples (based on indices)
             sample = self._reindex_sample(X[i])
-            if y[i] == 1:
-                self._discriminators[y[i]].fit(sample)
+            self._discriminators[y[i]].fit(sample)
         return self
 
     def predict(self, X: BooleanArray) -> np.ndarray:
